4b15d5a6c7ee3d3d0400cb515a4d23f2cdf80556
David <50298235+HiThere157@users.noreply.github.com>
2023-02-12 14:54:16 +0100

    init

---
+++ UDF.cs
@@ -0,0 +1,24 @@
+﻿using ExcelDna.Integration;
+
+namespace ExcelAD
+{
+    public class UDF
+    {
+        [ExcelFunction(Description = "First Desc")]
+        public static string SayHello(
+            [ExcelArgument(Description = "Name")] string name
+        )
+        {
+            return "Hello " + name;
+        }
+
+        [ExcelFunction(Description = "Second Desc")]
+        public static string SayBye(
+            [ExcelArgument(Description = "Name")]
+            string name
+        )
+        {
+            return "Hello " + name;
+        }
+    }
+}
