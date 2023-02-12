91b5746cddcb08f6fa43076d588be0cc7c5ea7bb
David <50298235+HiThere157@users.noreply.github.com>
2023-02-12 18:17:23 +0100

    ribbon

---
+++ Ribbon/RibbonController.cs
@@ -0,0 +1,14 @@
+﻿using ExcelDna.Integration.CustomUI;
+using System.Runtime.InteropServices;
+
+namespace ExcelAD.Ribbon
+{
+    [ComVisible(true)]
+    public class RibbonController : ExcelRibbon
+    {
+        public void OnLogonPressed(IRibbonControl control)
+        {
+            MessageBox.Show("Hello from control " + control.Id);
+        }
+    }
+}
